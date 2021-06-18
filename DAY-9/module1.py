ea52880be239dea5ef5a280f35a1cfcb9a0ce71a
Snehareddy124 <85571263+Snehareddy124@users.noreply.github.com>
2021-06-18 18:39:10 +0530

    Add files via upload

---
+++ DAY-9/module1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# import emoji
+# 
+# import pyttsx3
+# 
+# import camelcase
+# 
+# 
+# 
+# def call(s):
+#     s=s
+#     if s != s.lower() and s != s.upper() and "_" not in s:
+#         
+#         return emoji.emojize("It is in camel Case :thumbsup:", use_aliases=True)
+#     
+#     else:
+#         p=pyttsx3.init()
+#         y=camelcase.CamelCase()
+#         z=y.hump(s)
+#         p.say("Successfully converted string into camel Case")
+#         p.runAndWait()
